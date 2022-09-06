85f43b5f37b816e8e6dd4b5ace028e8cf4027ee4
midbel <brun.nicolas21@gmail.com>
2022-09-06 20:50:31 +0200

    refactor interp ensemble

---
+++ stdlib/interp.go
@@ -0,0 +1,135 @@
+package stdlib
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/midbel/gotcl/env"
+	"github.com/midbel/gotcl/stdlib/argparse"
+	"github.com/midbel/slices"
+)
+
+type InterpHandler interface {
+	Interpreter
+	RegisterInterpreter([]string, bool) (string, error)
+	UnregisterInterpreter([]string) error
+	LookupInterpreter(paths []string) (InterpHandler, error)
+	InterpretersList() []string
+	IsSafe() bool
+}
+
+type interpHandleFunc func(InterpHandler, []env.Value) (env.Value, error)
+
+func MakeInterp() Executer {
+	e := Ensemble{
+		Name: "interp",
+		List: []Executer{
+			Builtin{
+				Name:  "create",
+				Arity: 1,
+				Options: []argparse.Option{
+					{
+						Name:  "safe",
+						Flag:  true,
+						Value: env.False(),
+						Check: argparse.CheckBool,
+					},
+				},
+				Run: wrapInterpFunc(createInterp),
+			},
+			Builtin{
+				Name:  "delete",
+				Arity: 1,
+				Run:   wrapInterpFunc(deleteInterp),
+			},
+			Builtin{
+				Name:  "issafe",
+				Arity: 1,
+				Run:   wrapInterpFunc(isSafeInterp),
+			},
+			Builtin{
+				Name:     "eval",
+				Variadic: true,
+				Run:      wrapInterpFunc(evalInterp),
+			},
+			Builtin{
+				Name:  "children",
+				Arity: 1,
+				Run:   wrapInterpFunc(childrenInterp),
+			},
+		},
+	}
+	sort.Slice(e.List, func(i, j int) bool {
+		return e.List[i].GetName() < e.List[j].GetName()
+	})
+	return e
+}
+
+func createInterp(i InterpHandler, args []env.Value) (env.Value, error) {
+	paths, err := env.ToStringList(slices.Fst(args))
+	if err != nil {
+		return nil, err
+	}
+	safe, err := i.Resolve("safe")
+	if err != nil {
+		return nil, err
+	}
+	val, err := i.RegisterInterpreter(paths, env.ToBool(safe))
+	return env.Str(val), err
+}
+
+func deleteInterp(i InterpHandler, args []env.Value) (env.Value, error) {
+	paths, err := env.ToStringList(slices.Fst(args))
+	if err != nil {
+		return nil, err
+	}
+	return nil, i.UnregisterInterpreter(paths)
+}
+
+func isSafeInterp(i InterpHandler, args []env.Value) (env.Value, error) {
+	paths, err := env.ToStringList(slices.Fst(args))
+	if err != nil {
+		return nil, err
+	}
+	i, err = i.LookupInterpreter(paths)
+	if err != nil {
+		return nil, err
+	}
+	return env.Bool(i.IsSafe()), nil
+}
+
+func evalInterp(i InterpHandler, args []env.Value) (env.Value, error) {
+	paths, err := env.ToStringList(slices.Fst(args))
+	if err != nil {
+		return nil, err
+	}
+	i, err = i.LookupInterpreter(paths)
+	if err != nil {
+		return nil, err
+	}
+	return i.Execute(strings.NewReader(slices.Snd(args).String()))
+}
+
+func childrenInterp(i InterpHandler, args []env.Value) (env.Value, error) {
+	paths, err := env.ToStringList(slices.Fst(args))
+	if err != nil {
+		return nil, err
+	}
+	i, err = i.LookupInterpreter(paths)
+	if err != nil {
+		return nil, err
+	}
+	list := i.InterpretersList()
+	return env.ListFromStrings(list), nil
+}
+
+func wrapInterpFunc(do interpHandleFunc) CommandFunc {
+	return func(i Interpreter, args []env.Value) (env.Value, error) {
+		is, ok := i.(InterpHandler)
+		if !ok {
+			return nil, fmt.Errorf("interpreter can not handle interpreter children")
+		}
+		return do(is, args)
+	}
+}
